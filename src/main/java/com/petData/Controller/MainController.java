9ec009438c24ea462086809f3646fec91d99a6f5
hjyeon-n <hyunjy25@gmail.com>
2020-08-15 20:41:45 +0900

    [UPDATE] MainController

--- src/main/java/com/petData/Controller/MainController.java
+++ src/main/java/com/petData/Controller/MainController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	@RequestMapping("/") 
-	public String index() { 
-		return "index"; 
-	}
+//	@RequestMapping("/") 
+//	public String index() { 
+//		return "index"; 
+//	}
 	
 	@RequestMapping("/generator") 
 		public String generator() {
